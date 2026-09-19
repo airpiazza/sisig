4f9514cc1aaa9c0e479572d9451d773683575f29
airpiazza <nick@piazzaonline.com>
2026-09-19 10:16:40 -0700

    input for form

--- src/app/pages/add.tsx
+++ src/app/pages/add.tsx
@@ -1,5 +1,8 @@
 export const Add = () => {
-  return <>
-        Add
-      </>;
+  return <div>
+  <label  htmlFor="item-name">
+    What ingredient would you like to add?
+  </label>
+  <input  id="item-name" name="itemName" type="text" />
+</div>
 };
