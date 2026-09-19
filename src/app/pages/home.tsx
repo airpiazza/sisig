0a47c1881bfda60d4f79f9e86721aff479e3be91
airpiazza <nick@piazzaonline.com>
2026-09-19 10:09:20 -0700

    adding item page

--- src/app/pages/home.tsx
+++ src/app/pages/home.tsx
@@ -1,5 +1,4 @@
 export const Home = () => {
-  // _Feel free to delete this element and its import_
   return <>
         <div>
           <input id="item" name="item" type="checkbox" value="pasta" />
--- src/worker.tsx
+++ src/worker.tsx
@@ -4,6 +4,7 @@ import { defineApp } from "rwsdk/worker";
 import { Document } from "@/app/document";
 import { setCommonHeaders } from "@/app/headers";
 import { Home } from "@/app/pages/home";
+import { Add } from "./app/pages/add";
 
 export type AppContext = {};
 
@@ -13,5 +14,8 @@ export default defineApp([
     // setup ctx here
     ctx;
   },
-  render(Document, [route("/", Home)]),
+  render(Document, [
+    route("/", Home),
+    route("/add", () => <Add />)
+  ]),
 ]);
