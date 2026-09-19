072f9bc8e18a5e286fdd37bc53b843f5a00731bf
airpiazza <nick@piazzaonline.com>
2026-09-19 10:21:40 -0700

    add button

--- src/app/pages/add.tsx
+++ src/app/pages/add.tsx
@@ -1,8 +1,12 @@
 export const Add = () => {
-  return <div>
+  return <><div>
   <label  htmlFor="item-name">
     What ingredient would you like to add?
   </label>
   <input  id="item-name" name="itemName" type="text" />
 </div>
+<button type="submit" data-module="govuk-button">
+  Save and continue
+</button>
+</>
 };
