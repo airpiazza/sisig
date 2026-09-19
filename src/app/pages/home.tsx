0c1efa9f549a74a93a5e1b56a8ea1d5336df0176
airpiazza <nick@piazzaonline.com>
2026-09-19 09:55:24 -0700

    make a checkbox

--- src/app/pages/home.tsx
+++ src/app/pages/home.tsx
@@ -1,4 +1,9 @@
 export const Home = () => {
   // _Feel free to delete this element and its import_
-  return <p>hello</p>;
+  return <div>
+        <input id="item" name="item" type="checkbox" value="pasta" />
+        <label htmlFor="item">
+          pasta
+        </label>
+      </div>;
 };
